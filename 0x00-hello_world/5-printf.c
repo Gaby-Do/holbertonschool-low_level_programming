f807b40b2dd990b22fd31dc0d2092d8022477db1
Gaby-Do <eme.gaby.do@gmail.com>
2021-09-30 10:56:47 -0700

    5-printf.c-1

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - imprime una frase
+ *
+ * Description: imprime una frase
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}

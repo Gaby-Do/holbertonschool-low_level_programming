17eb3eb79c544e56de5cf3cb77e8dae1e3ad84bc
Gaby-Do <eme.gaby.do@gmail.com>
2021-09-30 10:34:07 -0700

    4-puts.c-1}

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - imrpimir una frase
+ * return 0
+ */
+
+int main(void)
+{
+	char a[55] = "\"Programming is like building a multilingual puzzle";
+
+	puts(a);
+
+	return (0);
+}

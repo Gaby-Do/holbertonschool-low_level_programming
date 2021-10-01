12edf0a63998e9160d4d71f21b6004ec6bf0a99d
Gaby-Do <eme.gaby.do@gmail.com>
2021-09-30 17:53:28 -0700

    101-3

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,12 +6,12 @@
  *
  * Description: imprime una frase
  *
- * Return: 0
+ * Return: 1
  */
 
 int main(void)
 {
 	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 
-	return (0);
+	return (1);
 }

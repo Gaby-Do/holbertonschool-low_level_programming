30bb9bfdfa1abd608c0417183d9c0d05beb0eae6
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-01 19:47:24 -0700

    102-1

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+*main - this is main
+*
+*Description: this is main
+*
+*Return: 0
+ */
+
+int main(void)
+{
+	int a;
+	int b;
+
+	for (a = 0; a < 98; a++)
+	{
+		for (b = a + 1; b <= 99; b++)
+		{
+			putchar((a / 10) + '0');
+			putchar((a % 10) + '0');
+			putchar(32);
+			putchar((b / 10) + '0');
+			putchar((b % 10) + '0');
+
+			if (a == 98 && b == 99)
+				continue;
+			putchar(44);
+			putchar(32);
+			}
+		}
+	putchar('\n');
+	return (0);
+}

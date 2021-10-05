2db0756b20ca86dd212c05a5ad4c23091bafd1de
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-05 14:51:07 -0700

    task 8-3

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -16,8 +16,13 @@ void jack_bauer(void)
 	int d;
 
 	for (a = 0; a < 3; a++)
+	{
 		for (b = 0; b < 10; b++)
+		{
+			if (a == 2 && b > 3)
+				break;
 			for (c = 0; c < 6; c++)
+			{
 				for (d = 0; d < 10; d++)
 				{
 					_putchar (a + '0');
@@ -27,4 +32,7 @@ void jack_bauer(void)
 					_putchar (d + '0');
 					_putchar ('\n');
 				}
+			}
+		}
+	}
 }

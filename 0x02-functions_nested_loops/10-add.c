82fc1b3713d195aa59157699c696904973522fc5
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-05 19:25:18 -0700

    task 10-3

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -12,27 +12,5 @@
 
 int add(int a, int b)
 {
-	int c;
-
-	c = 0;
-	c = a + b;
-	if (c >= 0 && c < 10)
-	{
-		_putchar ((c % 10) + '0');
-		_putchar ('\n');
-	}
-	if (c < 0 && c > -10)
-	{
-		c = c * (-1);
-		_putchar (45);
-		_putchar ((c % 10) + '0');
-		_putchar ('\n');
-	}
-	if (c > 9 || c < -9)
-	{
-		_putchar ((c / 10) + '0');
-		_putchar ((c % 10) + '0');
-		_putchar ('\n');
-	}
-	return (0);
+	return (a + b);
 }

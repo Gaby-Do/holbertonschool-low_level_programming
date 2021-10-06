4a1e9e4881a9dc317993d7c05022b52b236f5740
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-06 12:54:22 -0700

    9 otra vez

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,34 +10,26 @@
 
 void times_table(void)
 {
-	int a;
-	int b;
-	int c;
+	int a; /*number*/
+	int b; /*mul*/
+	int c; /*res*/
 
 	for (a = 0; a <= 9; a++)
 	{
 		for (b = 0; b <= 9; b++)
 		{
 			c = a * b;
-			if (c <= 9)
+			if (c >= 10)
 			{
-				_putchar (32);
-				_putchar (c + '0');
-				if (b == 9)
-				{
-					break;
-				}
-				_putchar (44);
+				_putchar((c / 10) + '0');
+			}
+			if (c < 10 && b > 0)
+			{
 				_putchar (32);
 			}
-			else
+			_putchar ((c % 10) + '0');
+			if (b < 9)
 			{
-				_putchar ((c / 10) + '0');
-				_putchar ((c % 10) + '0');
-				if (b == 9)
-				{
-					break;
-				}
 				_putchar (44);
 				_putchar (32);
 			}

a6a320ddbdf5cc71427377ed2b36abe0dfaef0d0
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-07 11:54:04 -0700

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,39 @@
+#include "main.h"
+#include "stdio.h"
+
+/**
+ *main - “Fizz-Buzz test”
+ *
+ *Return: 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 99; a++)
+	{
+		if ((a % 3) == 0)
+		{
+			printf("Fizz ");
+		}
+		else
+		{
+			if ((a % 5) == 0)
+			{
+				printf("Buzz ");
+			}
+			else
+			{
+				if (((a % 3) == 0) && (a % 5) == 0)
+				{
+					printf("FizzBuzz ");
+				}
+				else
+				{
+					printf("%d ", a);
+				}
+			}
+		}
+	}
+	printf("Buzz \n");
+}

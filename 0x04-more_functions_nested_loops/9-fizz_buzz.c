064f69268d5206ea5969e3b69b312cae9e13c359
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-07 12:23:32 -0700

    still looking for a solution to fizzbuzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -30,14 +30,7 @@ int main(void)
 				}
 				else
 				{
-					if (((a % 3) == 0) && (a % 5) == 0)
-					{
-						printf("FizzBuzz ");
-					}
-					else
-					{
-						printf("%d ", a);
-					}
+					printf("%d ", a);
 				}
 			}
 		}

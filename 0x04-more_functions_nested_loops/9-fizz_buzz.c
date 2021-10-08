81f64efee23717e4655bf04769c77d2a81a232b8
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-07 19:39:30 -0700

    fizzbuzz de nuevo

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -12,28 +12,23 @@ int main(void)
 
 	for (a = 1; a <= 99; a++)
 	{
-		if (((a % 3) == 0) && (a % 5) == 0)
+		if ((a % 15) == 0)
 		{
 			printf("FizzBuzz ");
 		}
-		else
+		if ((a % 3) == 0)
 		{
-			if ((a % 3) == 0)
-			{
-				printf("Fizz ");
-			}
-			else
-			{
-				if ((a % 5) == 0)
-				{
-					printf("Buzz ");
-				}
-				else
-				{
-					printf("%d ", a);
-				}
-			}
+			printf("Fizz ");
+		}
+		if ((a % 5) == 0)
+		{
+			printf("Buzz ");
+		}
+		if (((a % 3) != 0) && ((a % 5) != 0))
+		{
+			printf("%d ", a);
 		}
 	}
 	printf("Buzz\n");
+	return (0);
 }

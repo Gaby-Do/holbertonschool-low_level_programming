0f7b515f4207a981fb9a49a188f751573b715848
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-12 16:18:55 -0700

    task 6

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ *
+ * Description: prints half of a string
+ *
+ * @str: pointer
+ */
+void puts_half(char *str)
+{
+	int l = 0;/*largo del string*/
+	int a = 0;
+
+	while (str[l] != '\0')/*averiguo el largo*/
+	{
+		l++;
+	}
+	l--;
+	if (l % 2 == 0)
+	{
+		a = l / 2;
+		while (a <= l)
+		{
+			_putchar (str[a]);
+			a++;
+		}
+	}
+	else
+	{
+		a = (l - 1) / 2;
+		while (a <= l)
+		{
+			_putchar (str[a]);
+			a++;
+		}
+
+	_putchar (10);
+	}
+}

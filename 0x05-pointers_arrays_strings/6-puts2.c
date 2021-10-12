ac8dadf363dcc427bcbd36d8ff9ea89cec868229
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-12 15:55:10 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,11 +9,17 @@
 void puts2(char *str)
 {
 	int l = 0;
+	int a = 0;
 
 	while (str[l] != '\0')
 	{
-		_putchar (str[l]);
-		l = l + 2;
+		l++;
+	}
+	l--;
+	while (a <= l)
+	{
+		_putchar (str[a]);
+		a = a + 2;
 	}
 	_putchar (10);
 }

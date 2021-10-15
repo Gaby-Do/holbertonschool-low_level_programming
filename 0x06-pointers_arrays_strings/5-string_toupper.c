00b3e3338c9005b936f652ffa8b41a6d3353b832
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-15 09:30:03 -0700

    task 5

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -5,7 +5,18 @@
  * Description: changes all lowercase letters of a string to uppercase.
  *
  * @s: pointer
+ * Return: 0
  */
 char *string_toupper(char *s)
 {
+	int a = 0;
 
+	for (; s[a] != '\0'; a++)
+	{
+		if ((s[a] >= 97) && (s[a] <= 122))
+		{
+			s[a] = s[a] - 32;
+		}
+	}
+	return (s);
+}

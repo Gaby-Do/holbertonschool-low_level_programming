35de1a6162fbc730d73e5d6aa667c8995f08eff7
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-18 10:39:34 -0700

    task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -16,17 +16,16 @@ char *_strchr(char *s, char c)
 
 	if (c != '\0')
 	{
-		for (i = 0; s[i] != '\0'; i++)
+		for (i = 0; s[i] != '\0' && s[i] != c; i++)
 		{
-			if (s[i] == c)
-			{
-				p = &s[i];
-				break;
-			}
-			else
-			{
-				p = '\0';
-			}
+		}
+		if (s[i] == c)
+		{
+			p = &s[i];
+		}
+		else
+		{
+			p = '\0';
 		}
 	}
 	else

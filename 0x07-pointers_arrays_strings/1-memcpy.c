495023c6fc2b71e8a0c83f403c578ea83c27534f
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-18 09:40:07 -0700

    task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * _memcpy - copies memory area
+ ** _memcpy - copies memory area
  *
  * Description: copies memory area
  *
@@ -12,5 +12,11 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
+	unsigned int i;
 
+	for (i = 0; i < n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
 }

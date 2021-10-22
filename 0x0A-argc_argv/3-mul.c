f9bd4023009b4ecec2d76212b593b6b8ef25afe5
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-22 08:19:35 -0700

    corrigiendo task 2

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - multiplies two numbers.
+ *
+ * Description: multiplies two numbers.
+ *
+ * @argc: integer
+ * @argv: pointer
+ * Return: integer
+ */
+int main(int argc, char **argv)
+{
+	int x = 1;
+	int i;
+
+	for (i = 1; i < argc; i++)
+	{
+		x = x * atoi(argv[i]);
+	}
+	printf("%d\n", x);
+	return (0);
+}

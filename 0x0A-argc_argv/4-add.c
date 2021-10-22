379ab55332f8a522fb1bba87a315134ddf7b5380
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-22 09:27:10 -0700

    task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main -  adds positive numbers
+ *
+ * Description:  adds positive numbers
+ *
+ * @argc: integer
+ * @argv: pointer
+ * Return: 0 or 1 if wrong
+ */
+int main(int argc, char **argv)
+{
+	int i;
+	int x;
+	int y = 0;
+
+	if (argc >= 3)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			x = atoi(argv[i]);
+			if (x == 0)
+			{
+				printf("%s\n", "Error");
+				return (1);
+			}
+			y += atoi(argv[i]);
+		}
+		printf("%d\n", y);
+	}
+	else
+	{
+		if (argc == 1)
+		{
+			printf("%d\n", 0);
+			return (0);
+		}
+	}
+	return (0);
+}

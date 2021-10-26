3d15d1909ade07d67f0f48129b8d930a5975f6c2
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-26 11:14:28 -0700

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -16,11 +16,7 @@ char *str_concat(char *s1, char *s2)
 	int i = 0;
 	char *s3;
 
-	if (s1 == NULL)
-		return (s2);
-	if (s2 == NULL)
-		return (s1);
-	if (s1 == NULL && s2 == NULL)
+	if (s1 == NULL || s2 == NULL)
 		return (NULL);
 	while (s1[x] != '\0')
 		x++;

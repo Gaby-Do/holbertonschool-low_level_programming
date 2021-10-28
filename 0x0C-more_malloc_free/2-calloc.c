656603612953aa5e54fed0232769ffceb32a2f66
Gaby-Do <eme.gaby.do@gmail.com>
2021-10-28 12:36:15 -0700

    task 2

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -15,10 +15,10 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	void *p;
 
 	if (nmemb == 0 || size == 0)
-		return (0);
+		return (NULL);
 	p = malloc(nmemb * size);
 	if (!p)
-		return (0);
+		return (NULL);
 	memset(p, 0, nmemb);
 	return (p);
 }

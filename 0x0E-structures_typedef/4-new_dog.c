8a4f8b0b4b2e0cfdf0e29785e789a9db28f379fa
Gaby-Do <eme.gaby.do@gmail.com>
2021-11-01 09:15:58 -0700

    task 1

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,14 @@
+#include "dog.h"
+/**
+ * *new_dog - function that creates a new dog.
+ *
+ * Description: function that creates a new dog.
+ *
+ * @name: pointer
+ * @age: float
+ * @owner: pointer
+ *
+ * Return: NULL if fail
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{

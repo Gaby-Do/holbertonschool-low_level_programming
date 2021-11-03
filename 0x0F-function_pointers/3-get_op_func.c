a43ccc9f3480f70188670ee9b7fd81566799479e
Gaby-Do <eme.gaby.do@gmail.com>
2021-11-03 15:43:46 -0700

    task 3

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "function_pointers.h"
+#include "3-calc.h"
+/**
+ * get_op_func -  function that selects the correct function to
+ * perform the operation asked by the user
+ * Description:  function that selects the correct function to
+ * perform the operation asked by the user
+ * @s: pointer to char
+ * Return: pointer to the function that corresponds
+ * to the operator given as a parameter
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (i < 5)
+	{
+		if (s[0] == ops[i].op[0])
+			return (ops[i].f);
+		i++;
+	}
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,68 @@
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * op_add - returns the sum of a and b
+ * Description: returns the sum of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+/**
+ * op_sub - returns the difference of a and b
+ * Description: returns the difference of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ * op_mul - returns the product of a and b
+ * Description: returns the product of a and b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ * op_div - returns the result of the division of a by b
+ * Description: returns the result of the division of a by b
+ * @a: integer
+ * @b: integer
+ * Return: integer
+ */
+int op_div(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	return (a / b);
+}
+/**
+ * op_mod - returns the remainder of the division of a by b
+ * Description: returns the remainder of the division of a by b
+ * @a: int
+ * @b: int
+ * Return: int
+ */
+int op_mod(int a, int b)
+{
+	if (b == 0)
+	{
+		printf("Error\n"),
+		exit(100);
+	}
+	return (a % b);
+}

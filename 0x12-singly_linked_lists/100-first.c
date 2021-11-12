3f35070a64031a9577420f7a977ac6fafa9cfd47
Gaby-Do <eme.gaby.do@gmail.com>
2021-11-11 17:20:09 -0800

    task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * constructor - executes before main
+ */
+void constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}

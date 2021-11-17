dd7e782e2e2a9941b84e255798ac3a5a13bd4e5b
Gaby-Do <eme.gaby.do@gmail.com>
2021-11-17 11:56:15 -0800

    fixing 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -20,7 +20,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		free(uno);
 		return (1);
 	}
-	while (i < (index - 1))
+	while (i < (index - 2))
 	{
 		uno = uno->next;
 		if (uno == NULL)

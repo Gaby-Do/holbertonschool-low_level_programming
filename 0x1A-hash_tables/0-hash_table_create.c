8ea1b9c88eccfe9a870805944b5089a51bfff717
Gaby-Do <eme.gaby.do@gmail.com>
2022-01-27 11:04:17 -0800

    task 0

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -6,11 +6,19 @@
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hashArray;
+	hash_table_t *new_table;
 
-	hashArray = malloc(sizeof(hash_table_t) * size);
-		if (!hashArray)
+	if (!size)
+		return (NULL);
+	new_table = malloc(sizeof(hash_table_t));
+		if (!new_table)
 			return (NULL);
-
-	return (hashArray);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+		if (!new_table->array)
+		{
+			free(new_table);
+			return (NULL);
+		}
+	new_table->size = size;
+	return (new_table);
 }
